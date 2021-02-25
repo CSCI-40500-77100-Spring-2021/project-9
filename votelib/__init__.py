1a3473656dc0e33288801f3b83cfbe9a6f3ac65f
andrewlin1368 <andrewlin1368@gmail.com>
2021-02-24 20:00:18 -0500

    fptp

--- votelib/__init__.py
+++ votelib/__init__.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
+import csv
+from io import StringIO
+
 
 def fptp(ballots: str):
-    return {"winner": "Nobody",
-            "tallies": { "Nobody": 0 },
-           }
+    f = StringIO(ballots)
+    reader = csv.reader(f, delimiter=',')
+    row1 = next(f)
+    row1 = row1.strip()
+    row1 = row1.split(",")
+    results = {}
+
+    for names in row1:
+        names = names.strip()
+        results[names] = 0
+
+    for line in reader:
+        index = line.index("x")
+        candidate = row1[index]
+        results[candidate] += 1
+
+    winner = max(results, key=results.get)
+
+    return {"winner": winner,
+            "tallies": results,
+            }
