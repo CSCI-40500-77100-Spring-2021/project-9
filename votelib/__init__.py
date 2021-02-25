f80b6d20000cf1d55cce0c513920278b3365e8c8
Yuna <>
2021-02-24 20:36:46 -0500

    Clean up fptp code a bit

--- votelib/__init__.py
+++ votelib/__init__.py
@@ -1,27 +1,29 @@
 # -*- coding: utf-8 -*-
+
 import csv
 from io import StringIO
 
-
 def fptp(ballots: str):
+    # create a csv reader from the string argument
     f = StringIO(ballots)
     reader = csv.reader(f, delimiter=',')
-    row1 = next(f)
-    row1 = row1.strip()
-    row1 = row1.split(",")
-    results = {}
 
-    for names in row1:
-        names = names.strip()
-        results[names] = 0
+    # get the first row, which has candidate names
+    row = next(f)
+    candidates = row.strip().split(',')
 
+    # set up the tally dictionary with every candidate having 0 votes
+    tallies = {}
+    for c in candidates:
+        tallies[c] = 0
+
+    # count votes based on index of x
     for line in reader:
-        index = line.index("x")
-        candidate = row1[index]
+        index = line.index('x')
+        candidate = candidates[index]
         results[candidate] += 1
 
+    # grab the key with the highest value
     winner = max(results, key=results.get)
 
-    return {"winner": winner,
-            "tallies": results,
-            }
+    return {'winner': winner, 'tallies': tallies}
