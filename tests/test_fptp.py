5912ce72ca057aa22c2cb1ca264e8b1b3ad3a4a6
Yuna <>
2021-03-23 19:49:19 -0400

    Update tests for FPTP ties

--- tests/test_fptp.py
+++ tests/test_fptp.py
@@ -10,6 +10,16 @@ class TestFPTP():
             ballots = f.read()
 
         results = fptp(ballots)
-        assert results["winner"] == "Woof Woofington"
+        assert results["winners"] == {"Woof Woofington"}
         assert results["tallies"]["Woof Woofington"] == 4
         assert results["tallies"]["Meow Meowington"] == 3
+
+    def test_fptp_tie(self):
+        with open("tests/data/fptp_tie.csv") as f:
+            ballots = f.read()
+
+        results = fptp(ballots)
+        assert results["winners"] == {"Woof Woofington", "Meow Meowington"}
+        assert results["tallies"]["Woof Woofington"] == 2
+        assert results["tallies"]["Meow Meowington"] == 2
+        assert results["tallies"]["Boaty McBoatface"] == 1
