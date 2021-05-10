1e974759bc68a2b9c628cbdd1e9c8ffb1e091777
andrewlin1368 <andrewlin1368@gmail.com>
2021-05-09 21:40:48 -0400

    need help

--- telemetry/app.py
+++ telemetry/app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from votelib import
 
 app = Flask(__name__)
+
 
 # The endpoint to record metrics.
 # Method: POST
@@ -13,3 +15,18 @@ app = Flask(__name__)
 @app.route("/record")
 def record():
     return "Hello World"
+
+
+@app.route('/get/voting/results', methods=['POST'])
+def get_voting_results():
+    values = request.get_json()
+    required = ['voting_method']
+    if not all(keys in values for keys in required):
+        return 'Missing request info', 400
+    voting = values['voting_method']
+    if voting == "FPTP":
+        result = fptp()
+    else:
+        result = irv()
+    response = {result}
+    return jsonify(response), 201
