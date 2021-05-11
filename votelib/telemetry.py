3217dcada30c29650b43bc6ca9dfd368e177af15
ZhongweiL <zhongweili95@gmail.com>
2021-05-11 17:21:24 -0400

    Implement POST request to telementry server

--- votelib/telemetry.py
+++ votelib/telemetry.py
@@ -1,7 +1,10 @@
-SERVER_URL = "1.1.1.1"
+import requests
+
+SERVER_URL = "143.244.222.9"
 RECORD_ENDPOINT = "/record"
 
 # Make a request to the telemetry server using the requests library.
 # It's gonna be a POST request to /record.
-def record_usage():
-    pass
+def record_usage(method, size, duration_ms):
+    data = {"method": method, "size": size, "duration_ms": duration_ms}
+    requests.post("http://" + SERVER_URL + RECORD_ENDPOINT, data = data)
