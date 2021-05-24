1c0c069e217d345c1d2c2a72d7501f44b6cdf569
andrewlin1368 <andrewlin1368@gmail.com>
2021-05-23 20:46:03 -0400

    fixed error

--- votelib/irv.py
+++ votelib/irv.py
@@ -3,7 +3,7 @@ import csv
 
 def irv():
     # create a list from csv
-    with open(r"C:\Users\andrew\Desktop\405\project-9\tests\data\irv.csv", newline='') as f:
+    with open("tests\data\irv.csv", newline='') as f:
         reader = csv.reader(f)
         data = list(reader)
 
